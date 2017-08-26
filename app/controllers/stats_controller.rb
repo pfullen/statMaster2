631ba0b925889e99de8ce564f19d65d279af3197
pete <pete@localhost.localdomain>
2017-08-26 18:01:05 -0400

    fixed apikeu

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -5,7 +5,10 @@ require 'net/http'
 require 'openssl'
 
 def index
-url = URI("https://api.sportradar.us/mlb-t6/games/2014/REG/schedule.json?api_key=rsg2ehj4293rcr9x794dv7zr")
+api_key = ENV['api_key']
+##url = URI("https://api.sportradar.us/mlb-t6/games/2014/REG/schedule.json?api_key=rsg2ehj4293rcr9x794dv7zr")
+#url = URI("https://api.sportradar.us/mlb-t6/league/hierarchy.json?api_key=rsg2ehj4293rcr9x794dv7zr")
+url = URI("https://api.sportradar.us/mlb-t6/league/depth_charts.json?api_key=" + api_key)
 
 http = Net::HTTP.new(url.host, url.port)
 http.use_ssl = true
@@ -17,8 +20,13 @@ response = http.request(request)
 puts response.read_body
 
 @response = JSON.parse(response.read_body)
+@response = @response['teams'] #name => "Braves")  #[0]['name']
 
 
+@response2 = JSON.parse(response.read_body)
+#@market = @response['games']
+
+@api = ENV['api_key']
 
 
 	end
